62db4e0ae8fe237b44273b2bd3e82d993336a299
StivenLou <lostiven89@gmail.com>
2022-10-12 15:27:15 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    echo "hello Word";
+});
+
+Route::get('/name/{name}', function ($name) {
+    return "Hello " . $name ;
+});
+
+Route::get('/data/{data?}', function ($data = "Kosong") {
+    return "Isi Parameter " . $data ;
+});
+
+Route::get('/name/{name}', function ($name) {
+    return "Hello " . $name ;
+})->where('name', '[A-Za-z]+');
+
+Route::get('/nama/{nama}/{nrp}', function ($nama,$nrp) {
+    return "Nama " . $nama . "<br>" . "NRP " . $nrp ;
+})->where('nama', '[A-Za-z]+') ->where('nrp', '[0-9]+') ;
